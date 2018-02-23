0e003323e0ceb35ae1a0da8143d0c904dd401bab
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-22 16:34:22 -0800

    documentation

--- melanie-lab/server.js
+++ melanie-lab/server.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const http = require('http');
-const Kitteh = require('./model/kitteh.js');
 const Router = require('./lib/router.js');
 const kittehRouter = require('./route/kitteh-route.js');
-const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
